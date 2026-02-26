26ec0080ef5943f6bf215749c3e299f78137e251
Somaya Ayman <somaya189ayman@gmail.com>
2026-02-27 01:49:22 +0200

    add simple login test and include auth module in django

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+def login(request):
+    return render(request, 'registration/login.html', {})
 
 # Create your views here.
