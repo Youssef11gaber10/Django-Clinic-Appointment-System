3f99f7024dc41a937ee4a3cb4e228dfe5479dff6
Somaya Ayman <somaya189ayman@gmail.com>
2026-02-27 04:15:22 +0200

    create custom user app model

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.contrib.auth.models import User
+from .models import UserApp as User     
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'role']
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.shortcuts import reverse , get_object_or_404
 
-# Create your models here.
+class UserApp(AbstractUser):
+    role = models.CharField(max_length=20, choices=[('doctor', 'Doctor'), 
+                                                    ('patient', 'Patient'), 
+                                                    ('admin', 'Admin'), 
+                                                    ('receptionist', 'Receptionist')], default='patient')
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.username
