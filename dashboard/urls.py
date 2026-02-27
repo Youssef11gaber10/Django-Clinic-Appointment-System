a8b51a9f1f8e40335573bcf5120ee2aece743687
sherif-abdelsalam <sherif.abdelsalam.111@gmail.com>
2026-02-27 17:49:45 +0200

    added patient overview section with fake data

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('patient/',  views.patient_dashboard, name='patient_dashboard'),
+    path('patient/',  views.patient_dashboard_overview, name='patient_dashboard'),
 ]
