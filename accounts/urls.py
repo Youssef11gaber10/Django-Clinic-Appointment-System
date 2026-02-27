bc29e57212442fd958f4b88854a42f8ea111ec43
Somaya Ayman <somaya189ayman@gmail.com>
2026-02-27 03:23:14 +0200

    add simple sign up page and test signup and login flow

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been created successfully. You can now log in.')
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            #return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password.')
     return render(request, 'registration/login.html', {})
 
 # Create your views here.
