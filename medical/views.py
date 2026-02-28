58921a9aec426a9766fb3b8a9c3ddaa6218ac90b
gaber <gaber@localhost.localdomain>
2026-02-28 10:40:50 +0200

    make prescription and requested test functionalities

--- medical/views.py
+++ medical/views.py
@@ -95,7 +95,150 @@ def consultation_detail(request, pk):
 #     return render(request, "medical/consultation_confirm_delete.html", {"object": consultation})
 
 
+#prescription views 
 
+def prescription_create(request, consultation_id):
+
+    if not request.user.is_authenticated:
+            return redirect("login")
+    consultation = get_object_or_404(Consultation, pk=consultation_id)
+
+    # if consultation.appointment.slot.doctor.user != request.user:
+    #     return redirect("login")
+
+    if request.method == "POST":
+        form = PrescriptionItemForm(request.POST)
+        if form.is_valid():
+            prescription = form.save(commit=False)
+            prescription.consultation = consultation # this for add id of consultation
+            prescription.save()
+            return redirect("consultation_detail", pk=consultation.pk)
+    else:
+        form = PrescriptionItemForm()
+
+    return render(request, "medical/add_prescription.html", {"form": form})
+
+
+def prescription_edit(request, pk):
+
+    prescription = get_object_or_404(PrescriptoinItem, pk=pk)
+    if not request.user.is_authenticated:
+    
+        return redirect("login")
+
+    # if prescription.consultation.appointment.slot.doctor.user != request.user:
+    #     return redirect("login")
+
+    if request.method == "POST":
+        form = PrescriptionItemForm(request.POST, instance=prescription)
+        if form.is_valid():
+            form.save()
+            # return redirect("consultation_detail", pk=prescription.consultation.pk)
+            return redirect( "consultation_detail", pk=prescription.consultation_id)
+
+    else:
+
+        form = PrescriptionItemForm(instance=prescription)
+
+    return render(request, "medical/edit_prescription.html", {"form": form,"object": prescription })
+
+
+
+
+# def prescription_delete(request, pk):
+#     prescription = get_object_or_404(PrescriptoinItem, pk=pk)
+#     if not request.user.is_authenticated:
+#         return redirect("login")
+#     if prescription.consultation.appointment.slot.doctor.user != request.user:
+#         return HttpResponseForbidden()
+#     if request.method == "POST":
+#         consultation_id = prescription.consultation.pk
+#         prescription.delete()
+#         return redirect("consultation-detail", pk=consultation_id)
+#     return render(request, "medical/prescription_confirm_delete.html", {"object": prescription})
+
+
+
+
+def add_test(request, consultation_id):
+
+    if not request.user.is_authenticated:
+      return redirect("login")
+    
+    consultation = get_object_or_404(Consultation, pk=consultation_id)
+
+    # if consultation.appointment.slot.doctor.user != request.user:
+        #   return redirect("login")
+
+    if request.method == "POST":
+        form = RequestedTestForm(request.POST) 
+
+        if form.is_valid():
+            test = form.save(commit=False)
+            test.consultation = consultation    
+            test.save()
+
+            return redirect("consultation_detail", pk=consultation.pk)
+    else:
+        form = RequestedTestForm()
+
+    return render(request, "medical/add_test.html", {"form": form})
+    
+
+
+def edit_test(request, pk):
+
+    test = get_object_or_404(RequestedTest, pk=pk)
+
+    if not request.user.is_authenticated:
+        return redirect("login")
+
+    # if test.consultation.appointment.slot.doctor.user != request.user:
+    #     return redirect("login")
+
+    if request.method == "POST":
+        form = RequestedTestForm(request.POST, instance=test)
+    
+        if form.is_valid():
+            form.save()
+            return redirect("consultation_detail", pk=test.consultation.pk)
+    else:
+        form = RequestedTestForm(instance=test)
+
+    return render(request, "medical/test_edit.html", {"form": form,"object":test})
+
+
+
+# def test_delete(request, pk):
+# 
+# test = get_object_or_404(RequestedTest, pk=pk)
+# 
+# 
+# 
+# if not request.user.is_authenticated:
+# 
+    # return redirect("login")
+# 
+# if test.consultation.appointment.slot.doctor.user != request.user:
+# 
+    # return HttpResponseForbidden()
+# 
+# if request.method == "POST":
+# 
+    # consultation_id = test.consultation.pk
+# 
+    # test.delete()
+# 
+    # return redirect("consultation-detail", pk=consultation_id)
+# 
+# return render(request, "medical/test_confirm_delete.html", {"object": test})
+# 
+# 
+# 
+# 
+# 
+# 
+# 
 
 
 
