7853cd6514b70a4dbcb9a436a9748a5d05241e66
Nouran-Aly <nouran3ly@gmail.com>
2026-03-02 00:35:12 +0200

    added availability logic

--- scheduling/form.py
+++ scheduling/form.py
@@ -1,11 +1,27 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from .models import Availability
 
+User = get_user_model()
 
 # generate slots form
 # doctor - start date - end date
-
 class GenerateSlotsForm(forms.Form):
-    doctor = forms.ModelChoiceField(queryset=get_user_model().objects.filter(role="doctor"), label="Doctor")
+    doctor = forms.ModelChoiceField(queryset=User.objects.filter(role="doctor"), label="Doctor")
     start_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), label="Start Date")
     end_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), label="End Date")
+
+
+# availability form 
+class AvailabilityForm(forms.ModelForm):
+    class Meta:
+        model = Availability
+        fields = ('doctor', 'date_of_week', 'start_time', 'end_time', 'slot_duration', 'buffer_time')
+        widgets = {
+            'start_time' : forms.TimeInput(attrs={'type': 'time'}),
+            'end_time' : forms.TimeInput(attrs={'type': 'time'}),
+        }
+         
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['doctor'].queryset = User.objects.filter(role='doctor')
