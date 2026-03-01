981cb4b5bc082617b1865d7392ff84dc988d4afb
alaaateff <alaaatef3200@gmail.com>
2026-03-02 00:51:11 +0200

    modyfing views , models , add reschedule

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from appointments.views import book_appointment , cancel_appointment , check_in_appointment , complete_appointment , confirm_appointment,mark_no_show_appointment
+from appointments.views import book_appointment , cancel_appointment , check_in_appointment , complete_appointment , confirm_appointment,mark_no_show_appointment , slot_list,reschedule_page
 
 
-
+app_name = "appointments"
 urlpatterns = [
+    path("slots/",slot_list, name="slot_list"),
     # Patient
     path("book/<int:slot_id>/", book_appointment, name="book_appointment"),
     path("cancel/<int:appointment_id>/",cancel_appointment, name="cancel_appointment"),
@@ -13,4 +14,5 @@ urlpatterns = [
     path("no_show/<int:appointment_id>/", mark_no_show_appointment, name="mark_no_show"),
     # Doctor 
     path("complete/<int:appointment_id>/", complete_appointment, name="complete_appointment"),
+    path("reschedule/<int:appointment_id>/", reschedule_page, name="reschedule_appointment"),
 ]
